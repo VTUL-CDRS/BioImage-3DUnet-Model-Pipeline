7e56037dd7949028548756a8849be47a29ab7cdf
Linhan Wang <wanglinhan.pku@gmail.com>
2024-06-15 23:41:42 -0400

    reimplement clear function

--- postprocessing/clear.py
+++ postprocessing/clear.py
@@ -3,8 +3,11 @@ from scipy import ndimage as ndi
 import numpy as np
 import tifffile as tif
 from skimage import io
+import tyro
+from mpire import WorkerPool
+from functools import partial
 
-def clear_image(input_path, file_name, i, clear_size):
+def clear_image(i, input_path, file_name, clear_size):
 
     image_name = input_path+file_name + str(i) + ".tif"
 
@@ -13,14 +16,14 @@ def clear_image(input_path, file_name, i, clear_size):
     pores = tif.imread(image_name)
 
     # split
-    pores = morphology.opening(pores, morphology.ball(1))
+    pores = np.where(pores > 127, 255, 0).astype(np.uint8)
 
     # remove small objects
     pores = morphology.remove_small_objects(pores, min_size = clear_size)
     
     # save to tif
     output_file = input_path + "clear_" + str(clear_size) + "_" + file_name  + str(i) +  ".tif"
-    tif.imwrite(output_file, pores)
+    tif.imwrite(output_file, pores, compression='zlib')
     print("finish " + output_file)
 
 
@@ -28,75 +31,28 @@ def combine(input_path, file_name, number, clear_size):
 
     id_name = input_path + "clear_" + str(clear_size) + "_" + file_name
 
-    input_files = [id_name + f"{i}.tif" for i in range(0, number+1)]
+    input_files = [id_name + f"{i}.tif" for i in range(0, number)]
 
 
-    output_file = id_name + "conbine_" + str(clear_size) +".tif"
+    output_file = id_name + "combine_" + str(clear_size) +".tif"
 
-    print("start_conbine")
+    print("start_combine")
 
     first_tif = io.imread(input_files[0])
-    for i in range(1, number+1):
+    for i in range(1, number):
         img = io.imread(input_files[i])
         first_tif = np.concatenate((first_tif, img))
-    io.imsave(output_file, first_tif)
+    tif.imwrite(output_file, first_tif, compression='zlib')
 
-
-
-def clear_function(input_path,file_name,number,clear_size):
+def clear_function(input_path: str,number: int,clear_size:int,file_name: str="", ncores: int = 8):
     print("start clear:" + file_name)
-    for i in range(number + 1):
-        clear_image(input_path, file_name, i , clear_size)
+    clear_partial = partial(clear_image, input_path=input_path, clear_size=clear_size, file_name=file_name)
+    with WorkerPool(n_jobs=ncores) as pool:
+        pool.map(clear_partial, range(number), progress_bar=True)
     print("finish clear:" + file_name)
     combine(input_path, file_name, number, clear_size)
-    print("finish conbine:" + file_name)
+    print("finish combine:" + file_name)
 
     
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-1/"
-file_name = "Health-Cell-full-1-"
-number = 22
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-2/"
-file_name = "Health-Cell-full-2-"
-number = 20
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-3/"
-file_name = "Health-Cell-full-3-"
-number = 21
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-4/"
-file_name = "Health-Cell-full-4-"
-number = 17
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-5/"
-file_name = "Health-Cell-full-5-"
-number = 12
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-6/"
-file_name = "Health-Cell-full-6-"
-number = 7
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-7/"
-file_name = "Health-Cell-full-7-"
-number = 17
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
-
-input_path = r"/mnt/research-data/data/Health-Cell-cut/Health-Cell-full-8/"
-file_name = "Health-Cell-full-8-"
-number = 20
-clear_size = 400
-clear_function(input_path,file_name,number,clear_size)
+if __name__ == '__main__':
+    tyro.cli(clear_function)
