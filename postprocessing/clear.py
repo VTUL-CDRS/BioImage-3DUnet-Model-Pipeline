74e6aac0bc6e5b897d687a6e5f8655ba103690f3
Linhan Wang <wanglinhan.pku@gmail.com>
2024-06-23 15:02:47 -0400

    add threshold in args

--- postprocessing/clear.py
+++ postprocessing/clear.py
@@ -7,13 +7,15 @@ import tyro
 from mpire import WorkerPool
 from functools import partial
 
-def clear_image(i, input_path, file_name, clear_size):
+def clear_image(i, input_path, file_name, clear_size, threshold):
 
     image_name = input_path+file_name + str(i) + ".tif"
 
     print("process " + image_name)
 
     pores = tif.imread(image_name)
+
+    pores[pores < threshold] = 0
     
     out, num = morphology.label(pores, return_num=True, connectivity=2)
     ids, cnts = np.unique(out, return_counts=True)
@@ -25,21 +27,21 @@ def clear_image(i, input_path, file_name, clear_size):
     out = out.astype(np.uint8)
 
     # save to tif
-    output_file = input_path + "clear_" + str(clear_size) + "_" + file_name  + str(i) +  ".tif"
+    output_file = f'{input_path}clear_{clear_size}_{file_name}{i}_{threshold}.tif'
     tif.imwrite(output_file, out, compression='zlib')
     print("finish " + output_file)
 
 
-def combine(input_path, file_name, number, clear_size):
+def combine(input_path, file_name, number, clear_size, threshold):
 
-    id_name = input_path + "clear_" + str(clear_size) + "_" + file_name
+    id_name = input_path + "clear_" + str(clear_size) + "_" + file_name + '_' + str(threshold) + '_'
 
     input_files = [id_name + f"{i}.tif" for i in range(0, number)]
 
 
-    output_file = id_name + "conbine_" + str(clear_size) +".tif"
+    output_file = id_name + "combine_" + str(clear_size) +".tif"
 
-    print("start_conbine")
+    print("start_combine")
 
     first_tif = io.imread(input_files[0])
     for i in range(number):
@@ -47,14 +49,14 @@ def combine(input_path, file_name, number, clear_size):
         first_tif = np.concatenate((first_tif, img))
     tif.imwrite(output_file, first_tif, compression='zlib')
 
-def clear_function(input_path: str,number: int,clear_size:int,file_name: str="", ncores: int = 16):
+def clear_function(input_path: str,number: int,clear_size:int, threshold:int, file_name: str="", ncores: int = 16):
     print("start clear:" + file_name)
-    clear_partial = partial(clear_image, input_path=input_path, clear_size=clear_size, file_name=file_name)
+    clear_partial = partial(clear_image, input_path=input_path, clear_size=clear_size, file_name=file_name, threshold=threshold)
     with WorkerPool(n_jobs=ncores) as pool:
         pool.map(clear_partial, range(number), progress_bar=True)
     print("finish clear:" + file_name)
-    combine(input_path, file_name, number, clear_size)
-    print("finish conbine:" + file_name)
+    combine(input_path, file_name, number, clear_size, threshold)
+    print("finish combine:" + file_name)
 
     
 if __name__ == '__main__':
