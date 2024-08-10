867905084bbde07a4222d6accb36efd8f6920b23
Linhan Wang <wanglinhan.pku@gmail.com>
2024-08-10 19:01:17 -0400

    delete old training codes

--- training/make_exp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export EXP=exp_$@
-mkdir $EXP
-cp train_with_dynamic_cyclical_data_augmentation_random_sample_nonfix.py $EXP/
-cp augmentation.py $EXP/
-cp cfg.txt $EXP/
-cp one_dataset.sh $EXP/
